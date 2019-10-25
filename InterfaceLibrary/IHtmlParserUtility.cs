a9f0afcc23ae223b84a2c2598b32814f64bbece8
Chetan Garg <chetan1983@gmail.com>
2019-10-25 12:17:03 +1100

    Added an HTML Parser

---
+++ InterfaceLibrary/IHtmlParserUtility.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SympliSEOSolution.InterfaceLibrary
+{
+    public interface IHtmlParserUtility
+    {
+        string ParseAsXml(string html, string tagToRemove);
+    }
+}
